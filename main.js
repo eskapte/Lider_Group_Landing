02f919b93f1366d7e021bd04d01a0128d01a0d81
Sergey Sobol <eskapte@mail.ru>
2023-04-10 00:50:24 +0300

    finish

---
+++ main.js
@@ -0,0 +1,19 @@
+const carousels = document.querySelectorAll(".carousel");
+
+carousels.forEach(carousel => {
+  new Carousel(carousel, {
+    'slidesPerPage' : 1,
+    'preload' : '2',
+  });
+})
+
+const menuBurger = document.querySelector(".main-header__menu-burger");
+const body = document.body;
+const contacts = document.querySelector(".main-header__contacts");
+
+menuBurger.addEventListener("click", onMenuClicked);
+
+function onMenuClicked(evt) {
+  body.classList.toggle("menu-displayed");
+  contacts.classList.toggle("show");
+}
